92ccbef2a6679de636a289a3b08e83e494887b4d
ujjwal-207 <ujjwalnepal2060@gmail.com>
2025-04-22 23:07:25 +0545

    created logger

---
+++ src/mcqgenerator/logger.py
@@ -0,0 +1,16 @@
+
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE =f"{datetime.now().strftime('%Y-%m-%d')}.log"
+
+log_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
+# python logging module to log messages to a file and console
+logging.basicConfig(level=logging.INFO,
+        filename=LOG_FILE_PATH,
+        format='[%(asctime)s]%(lineno)d %(name)s - %(levelname)s - %(message)s'
+)
